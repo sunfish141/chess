eea41a7a2773c360041546ccfc71424dc0e869cd
sunfish141 <108562688+sunfish141@users.noreply.github.com>
2023-05-09 14:43:25 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -275,7 +275,6 @@ function possibleMoves(piece) {
         pieces[i].column == piece.column - 1 &&
         pieces[i].row == piece.row + 1
       ) {
-        console.log("EEEEE");
         if (pieces[i].color != piece.color) {
           possible.push({ row: piece.row + 1, column: piece.column - 1 });
         }
@@ -283,7 +282,6 @@ function possibleMoves(piece) {
         pieces[i].column == piece.column + 1 &&
         pieces[i].row == piece.row + 1
       ) {
-        console.log("EEEEE");
         if (pieces[i].color != piece.color) {
           possible.push({ row: piece.row + 1, column: piece.column + 1 });
         }
@@ -342,7 +340,6 @@ function possibleMoves(piece) {
       }
     }
   } else if (piece.piece == "queen") {
-    console.log("eeeee");
     possible = [];
     let hit = false;
     let addcounter = 1;
@@ -360,7 +357,6 @@ function possibleMoves(piece) {
               column: piece.column,
             });
           }
-          console.log("qweqwe");
           hit = true;
         }
       }
@@ -662,7 +658,7 @@ function possibleMoves(piece) {
     }
   }
   //}
-  else if (piece.piece == "bishop"){
+  else if (piece.piece == "bishop") {
     possible = [];
     let hit = false;
     let addcounter = 1;
@@ -779,14 +775,42 @@ function possibleMoves(piece) {
       });
       addcounter++;
     }
-  }
-  else if (piece.piece == "knight"){
-    possible = [{row: piece.row - 2, column: piece.column - 1}, {row: piece.row - 1, column: piece.column - 2}, {row: piece.row + 1, column: piece.column - 2}
-    , {row: piece.row + 2, column: piece.column - 1}, {row: piece.row + 2, column: piece.column + 1}, {row: piece.row + 1, column: piece.column + 2},
-    {row: piece.row - 1, column: piece.column + 2}, {row: piece.row - 2, column: piece.column + 1}];
+  } else if (piece.piece == "knight") {
+    possible = [
+      { row: piece.row - 2, column: piece.column - 1 },
+      { row: piece.row - 1, column: piece.column - 2 },
+      { row: piece.row + 1, column: piece.column - 2 },
+      { row: piece.row + 2, column: piece.column - 1 },
+      { row: piece.row + 2, column: piece.column + 1 },
+      { row: piece.row + 1, column: piece.column + 2 },
+      { row: piece.row - 1, column: piece.column + 2 },
+      { row: piece.row - 2, column: piece.column + 1 },
+    ];
+    let clear = false;
+    let cycle = 0;
+
+    while (cycle < possible.length && !clear) {
+      for (let j = 0; j < pieces.length; j++) {
+        if (
+          possible[cycle].row == pieces[j].row &&
+          possible[cycle].column == pieces[j].column &&
+          piece.color == pieces[j].color
+        ) {
+          possible.splice(cycle, 1);
+          cycle--;
+          break;
+          //issue
+        }
+      }
+
+      cycle++;
+      if (cycle == possible.length) {
+        clear = true;
+      }
+    }
   }
   //clear unwanted values
-  let clear = false;
+  clear = false;
   while (clear == false) {
     for (q = 0; q < possible.length; q++) {
       if (
@@ -795,7 +819,6 @@ function possibleMoves(piece) {
         possible[q].row < 0 ||
         possible[q].row > 7
       ) {
-        console.log(possible[q]);
         possible.splice(q, 1);
       } else {
         clear = true;
@@ -828,9 +851,6 @@ function getCursorPosition(c, event) {
     if (pieces[k].selected == true) {
       if (pieces[k].color == currentturn) {
         let moves = possibleMoves(pieces[k]);
-        console.log(pieces);
-        console.log(k);
-        console.log(moves);
         if (
           (moves.map((a) => a.row).includes(7 - piecey) &&
             moves.map((a) => a.column).includes(piecex)) == true
@@ -844,7 +864,6 @@ function getCursorPosition(c, event) {
               pieces.splice(z, 1);
             }
           }
-          console.log(pieces);
           for (j = 0; j < pieces.length; j++) {
             if (pieces[j].selected == true) {
               pieces[j].column = piecex;
@@ -855,12 +874,10 @@ function getCursorPosition(c, event) {
               }
             }
           }
-          console.log(k);
           movefound = true;
         }
         if (currentturn == "white" && movefound == true) {
           currentturn = "black";
-          console.log("switch");
         } else if (currentturn == "black" && movefound == true) {
           currentturn = "white";
         }
@@ -880,7 +897,6 @@ function getCursorPosition(c, event) {
       }
     }
   }
-  console.log("x: " + piecex + " y: " + piecey);
 }
 
 const c = document.querySelector("canvas");
